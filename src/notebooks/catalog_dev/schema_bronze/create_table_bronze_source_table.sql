5b636ee6ba2937231317a75250ae9eabc8fc752f
filipniziol <filipniziol>
2024-12-19 11:38:39 +0000

    Added code for create table

--- src/notebooks/catalog_dev/schema_bronze/create_table_bronze_source_table.sql
+++ src/notebooks/catalog_dev/schema_bronze/create_table_bronze_source_table.sql
@@ -1,1 +1,15 @@
 -- Databricks notebook source
+CREATE WIDGET TEXT catalog_name DEFAULT 'dev';
+CREATE WIDGET TEXT schema_name DEFAULT 'bronze';
+CREATE WIDGET TEXT table_name DEFAULT 'source_table';
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TABLE IDENTIFIER(:catalog_name || '.' || :schema_name ||  '.' || :table_name)
+(
+    id INT,
+    name STRING,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    status STRING
+);
